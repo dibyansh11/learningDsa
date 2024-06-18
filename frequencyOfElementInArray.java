e5b13369afbca8d289fe40ee54445a463686a230
dibyansh11 <94691328+dibyansh11@users.noreply.github.com>
2024-06-18 08:57:34 +0530

    count the frequency of  each elements in the array

---
+++ frequencyOfElementInArray.java
@@ -0,0 +1,20 @@
+public class frequencyOfElementInArray {
+    public static void main(String[] args) {
+        int a[] = {5,5,5,6,2,5,4,3,3,3,7,4,7,7,7};
+        int n = a.length;
+        for(int i= 0 ; i<n ; i++)
+        {
+            int cnt = 0 ; 
+            for(int  j = 0 ; j < n ; j++)
+            {
+                
+                if(a[j] == a[i])
+                {
+                    cnt++;
+                }
+            }
+            System.out.println("The number of elements of " + a[i] + " is "+cnt);
+        }
+    }
+    
+}
